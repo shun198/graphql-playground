da93d13eed5ce1e280e237d7ae2d0c244aca32dc
shun198 <shunhiroseluvmri@gmail.com>
2024-01-29 19:59:35 +0900

    👍 add context

--- hackernews/src/server.js
+++ hackernews/src/server.js
@@ -2,6 +2,10 @@ const { ApolloServer, gql } = require('apollo-server');
 const fs = require(`fs`);
 const path = require(`path`);
 
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 // HackerNewsのfeed
 let links = [
   {
@@ -36,6 +40,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバを起動中・・・`));
