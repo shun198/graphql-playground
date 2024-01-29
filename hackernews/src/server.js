b7a0bf267d7ca8c66f29c1a659bbb10f2057e2f8
shun198 <shunhiroseluvmri@gmail.com>
2024-01-29 19:28:27 +0900

    👍 create schema.graphql

--- hackernews/src/server.js
+++ hackernews/src/server.js
@@ -1,4 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
+const fs = require(`fs`);
+const path = require(`path`);
 
 // HackerNewsのfeed
 let links = [
@@ -8,23 +10,6 @@ let links = [
     url: 'www.udemy-graphql-tutorial.com',
   },
 ];
-// Graphqlのスキーマ定義
-const typeDefs = gql`
-  type Query {
-    info: String!
-    feed: [Link]!
-  }
-
-  type Mutation {
-    post(url: String!, description: String!): Link!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
 
 const resolvers = {
   Query: {
@@ -49,7 +34,7 @@ const resolvers = {
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
 });
 
